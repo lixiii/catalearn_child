4fc827f735b07bd3aa5c1ba2e3bfe0c5e013cb15
Li Xi <xl404@cam.ac.uk>
2017-07-22 13:06:39 +0800

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 apt-get update 
 apt-get -y upgrade 
-apt-get install -y python3-pip 
-pip3 install --upgrade setuptools pip 
-pip3 install jupyter
-pip3 install keras
+apt-get install -y \
+    nodejs  \
+    python3-pip 
+# pip3 install --upgrade setuptools pip 
+pip3 install \
+    keras   \
+    jupyter
 
 npm install
